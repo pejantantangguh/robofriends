6aa02515d09cc877f2e8bc1b8e2650acfcd63682
= <=>
2018-05-20 21:16:07 +1000

    Done Simple React App!!

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Card = (props) => {
     return (
         <div className='bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <h1>Robo Friends</h1>
             <img src={`https://robohash.org/${props.id}?200x200`} alt='robots' />
             <div>
                 <h2>{props.name}</h2>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import { robots } from './robots';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import '../containers/App.css';
 
 
 
@@ -11,12 +12,29 @@ class App extends React.Component {
         // STATE For SearchBox
         // STATE can change and affect our apps
         this.state = {
-                robots: robots,
+                robots: [],
                 searchField: ''
             }
             // But before we use state 
             // we have to change the way we use react component, we crete a class
+            // This is runned 1
+            console.log('constructor');
+        
         }
+        
+
+        componentDidMount() {
+            fetch('https://jsonplaceholder.typicode.com/users')
+            //Updating users by Fetching the data
+                .then(response => {
+                    return response.json();
+                })
+                .then(users => {
+                    this.setState({robots: users})
+                });
+            console.log('componentDidMount');
+        }
+        
 
         onSeachChange = (event) => {
             this.setState({searchField : event.target.value})
@@ -26,16 +44,23 @@ class App extends React.Component {
             const filteredRobots = this.state.robots.filter(robot => {
                 return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
             });
+            console.log('Render');
+            if(this.state.robots.length === 0 ){
+                return <h1> Loading </h1>
+            } else {
         return (
             <div className='tc'>
-                <h1> Robot Firends </h1>
+                <h1> Robot Friends </h1>
                 <SearchBox searchChange= {this.onSeachChange}/>
                 {/* CardList needs to know whats in SearchBox so we can filter it out 
                 We have to use STATE in our APP*/}
-                <CardList robots= {filteredRobots}/>
+                <Scroll>
+                    <CardList robots= {filteredRobots}/>
+                </Scroll>
             </div>
         );
     }
+}
     
 }
 
